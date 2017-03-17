1ff843eef19932a3f4a0fe26468cb285862e4bb1
Renaud Cousin <rcousin@octo.com>
2017-03-17 16:29:04 +0100

    Add dampling on send button appear

---
+++ Animations/View/Share/ShareButton.swift
@@ -0,0 +1,14 @@
+//
+//  ShareButton.swift
+//  Animations
+//
+//  Created by Octo-RCO on 17/03/2017.
+//  Copyright © 2017 Octo-RCO. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShareButton: UIView {
+  
+}
